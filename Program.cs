cf2ed3da7c09a39d4cf357ad2345b312b82f7c27
KEVIN LUCAS DE ANDRADE <kevin.lucas@cs.up.edu.br>
2025-04-11 20:57:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Saudacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicita ao usuário que digite o nome
+            Console.Write("Digite seu nome: ");
+            
+            // Lê o nome digitado pelo usuário
+            string nome = Console.ReadLine();
+
+            // Exibe a saudação personalizada
+            Console.WriteLine($"Olá, {nome}! Seja bem-vindo(a)!");
+        }
+    }
+}
