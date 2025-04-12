5f270c0d19c19f73f8cbd73daa77e47ac805f198
KEVIN LUCAS DE ANDRADE <kevin.lucas@cs.up.edu.br>
2025-04-11 21:13:16 -0300

    tirando comentario

--- Program.cs
+++ Program.cs
@@ -6,13 +6,10 @@ namespace Saudacao
     {
         static void Main(string[] args)
         {
-            // Solicita ao usuário que digite o nome
             Console.Write("Digite seu nome: ");
             
-            // Lê o nome digitado pelo usuário
             string nome = Console.ReadLine();
 
-            // Exibe a saudação personalizada
             Console.WriteLine($"Olá, {nome}! Seja bem-vindo(a)!");
         }
     }
